83981d229756499176763a00080416977b4bc64b
Surendar S <surendars@Surendars-MacBook-Pro.local>
2016-10-31 16:50:04 +0530

    completed

--- CatchEmAll/Plane.swift
+++ CatchEmAll/Plane.swift
@@ -7,3 +7,133 @@
 //
 
 import Foundation
+
+
+class Plane:Vehicle {
+
+
+    let maxAltitude:Double
+    var altitude:Double
+    var inFlight:Bool {
+        return altitude != 0 && speed != 0
+    }
+
+    init(name: String, weight: Double, maxSpeed: Double, maxAltitude:Double) {
+        self.maxAltitude = maxAltitude
+        self.altitude = 0
+        super.init(name: name, weight: weight, maxSpeed: maxSpeed)
+    }
+
+    
+    func takeOff(){
+    
+        if !self.inFlight {
+            self.speed = self.speed + self.maxSpeed*0.1
+            self.altitude = self.altitude + self.maxAltitude * 0.1
+            
+        }
+    
+    }
+    
+    
+    func land(){
+        self.altitude = 0
+        self.speed = 0
+    }
+    
+    func climb(){
+    
+        if self.inFlight {
+            
+            
+            let alt = self.altitude + self.maxAltitude * 0.1
+            
+
+            if alt <= self.maxAltitude {
+                self.altitude = alt
+            }else {
+                self.altitude = self.maxAltitude
+            }
+            
+            self.decelerate()
+            
+            
+        }
+    
+    }
+    
+    
+    func dive(){
+        
+        if self.inFlight {
+            
+            
+            let alt = self.altitude - self.maxAltitude * 0.1
+            
+            if alt > 0 {
+                self.altitude = alt
+            }else {
+                self.altitude = 0.0
+            
+            }
+            
+            self.accelerate()
+            
+            
+        }
+        
+    }
+    
+    func bankRight(){
+    
+    
+        if self.inFlight {
+        
+            var ang = self.heading + 45
+            if ang > 360 {
+                ang = ang - 360
+            }
+            
+            
+            self.heading = ang
+            
+            let spe = self.speed - self.speed * 0.1
+            
+            if spe > 0 {
+                self.speed = spe
+            }
+            
+        
+        }
+        
+    
+    }
+    
+    func bankLeft(){
+        
+        
+        if self.inFlight {
+            
+            var ang = self.heading - 45
+            if ang < 0 {
+                ang = 360 + ang
+            }
+            
+            
+            self.heading = ang
+            
+            let spe = self.speed - self.speed * 0.1
+            
+            if spe > 0 {
+                self.speed = spe
+            }
+            
+            
+        }
+        
+        
+    }
+
+    
+    
+}
