70361fbda5e70e515877bfea860d5178bf4ca1b8
Ben Bernstein <bb@burningflowe.rs>
2016-12-29 01:13:32 -0500

    finish, cool to learn didset too

--- CatchEmAll/Plane.swift
+++ CatchEmAll/Plane.swift
@@ -7,3 +7,76 @@
 //
 
 import Foundation
+
+class Plane : Vehicle {
+    let maxAltitude : Double
+    var altitude: Double {
+        didSet {
+            if altitude < 0.0 {
+                altitude = 0.0
+            }
+            
+            if altitude > maxAltitude {
+                altitude = maxAltitude
+            }
+        }
+    }
+    var inFlight : Bool {
+        guard altitude > 0 else {return false}
+        guard speed > 0 else {return false}
+        return true
+    }
+    
+    init(name: String, weight: Double, maxSpeed: Double, maxAltitude: Double) {
+        self.maxAltitude = maxAltitude
+        self.altitude = 0.0
+        super.init(name: name, weight: weight, maxSpeed: maxSpeed)
+    }
+    
+    func takeOff() {
+        guard !inFlight else {return}
+        speed = maxSpeed * 0.1
+        altitude = maxAltitude * 0.1
+    }
+    
+    func land() {
+        speed = 0.0
+        altitude = 0.0
+    }
+    
+    
+    func climb() {
+       guard inFlight else {return}
+      
+        altitude += 0.1 * maxAltitude
+        super.decelerate()
+        
+        
+    }
+
+
+    
+    func dive() {
+        guard inFlight else {return}
+       
+        altitude -= 0.1 * maxAltitude
+        super.accelerate()
+        
+        
+    }
+    
+    func bankRight() {
+        guard inFlight == true else {return}
+        heading += 45
+        speed *= 0.9
+        
+    }
+    
+    func bankLeft() {
+        guard inFlight == true else {return}
+        heading -= 45
+        speed *= 0.9
+        
+    }
+
+}
--- CatchEmAll/Vehicle.swift
+++ CatchEmAll/Vehicle.swift
@@ -7,3 +7,80 @@
 //
 
 import Foundation
+
+class Vehicle {
+    var name : String
+    var weight : Double
+    var maxSpeed : Double
+    var speed : Double {
+        didSet {
+            if speed > megaSpeed {
+                speed = maxSpeed
+            }
+
+            
+            while speed < 0 {
+                speed = 0
+            }
+        }
+    }
+    
+    var megaSpeed: Double {
+        return maxSpeed
+    }
+    
+    var heading : Double {
+        didSet {
+            while heading < 0.0 {
+                heading += 360.0
+            }
+            
+            while heading > 360.0 {
+                heading -= 360.0
+            }
+        }
+    }
+    
+    init(name: String, weight: Double, maxSpeed: Double) {
+        self.name = name
+        self.weight = weight
+        self.maxSpeed = maxSpeed
+        self.speed = 0
+        self.heading = 0
+    }
+    
+    func goFast() {
+        speed = maxSpeed
+    }
+    
+    func halt() {
+        speed = 0
+    }
+    
+    func accelerate() {
+        guard speed < maxSpeed else {return}
+        speed = speed + 0.1 * maxSpeed
+    }
+
+    func decelerate() {
+        guard speed > 0 else {return}
+        speed = speed - 0.1 * maxSpeed
+    }
+    
+    func turnRight() {
+         guard speed > 0 else {return}
+         heading += 90
+         speed = speed * 0.5
+        
+    }
+    
+    func turnLeft() {
+        guard speed > 0 else {return}
+        heading -= 90
+        speed *= 0.5
+        
+    }
+    
+   
+
+}
