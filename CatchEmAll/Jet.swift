3c1fa3e640b067debe267f256a565a6a7268c49e
KircheisHe <hechenzhang@outlook.com>
2017-01-04 22:23:36 -0800

    Complete Task

--- CatchEmAll/Jet.swift
+++ CatchEmAll/Jet.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+
+class Jet : Plane {
+    override func climb() {
+        if (inFlight) {
+            self.altitude += 0.2*self.maxAltitude
+            if (self.altitude > self.maxAltitude) {
+                self.altitude = self.maxAltitude
+            }
+            
+            decelerate()
+        }
+    }
+    
+    override func dive() {
+        if (inFlight) {
+            self.altitude -= 0.2*self.maxAltitude
+            if (self.altitude < 0) {
+                self.altitude = 0
+            }
+            
+            accelerate()
+        }
+    }
+    
+    func afterburner() {
+        if (self.speed == self.maxSpeed) {
+            self.speed = self.maxSpeed * 2
+        }
+    }
+}
--- CatchEmAll/Vehicle.swift
+++ CatchEmAll/Vehicle.swift
@@ -7,3 +7,69 @@
 //
 
 import Foundation
+
+enum Transmission {
+    case Automatic
+    case Manual
+}
+
+class Vehicle {
+    let name : String;
+    let weight : Double;
+    let maxSpeed : Double;
+    
+    var speed : Double;
+    var heading : Double;
+    
+    init(name : String, weight : Double, maxSpeed : Double) {
+        self.name = name;
+        self.weight = weight;
+        self.maxSpeed = maxSpeed;
+        self.speed = 0.0;
+        self.heading = 0.0;
+    }
+    
+    func goFast() {
+        self.speed = self.maxSpeed
+    }
+    
+    func halt() {
+        self.speed = 0;
+    }
+    
+    func accelerate() {
+        self.speed += self.maxSpeed*0.1
+        if (self.speed > self.maxSpeed) {
+            self.speed = self.maxSpeed
+        }
+    }
+    
+    func decelerate() {
+        self.speed -= self.maxSpeed*0.1
+        if (self.speed < 0) {
+            self.speed = 0;
+        }
+    }
+    
+    func turnRight() {
+        if (self.speed == 0) {
+            return
+        }
+        self.speed = self.speed / 2;
+        self.heading += 90;
+        if (self.heading > 360) {
+            self.heading -= 360
+        }
+    }
+    
+    func turnLeft() {
+        if (self.speed == 0) {
+            return
+        }
+        self.speed = self.speed / 2;
+        self.heading -= 90;
+        if (self.heading < 0) {
+            self.heading += 360
+        }
+    }
+}
