1b30e2d1563f07abbc39eba0d4782b693bb1b46e
Marc Aupont <marc@digimarktech.com>
2017-02-09 13:26:11 -0500

    Completed Classes and Inheritance Lab with all tests passing

--- CatchEmAll/Plane.swift
+++ CatchEmAll/Plane.swift
@@ -7,3 +7,124 @@
 //
 
 import Foundation
+
+class Plane: Vehicle {
+    
+    let maxAltitude: Double
+    var altitude: Double
+    
+    var inFlight: Bool {
+        
+        get {
+            
+            if altitude > 0 && speed > 0 {
+                
+                return true
+                
+            }
+            
+            return false
+        }
+    }
+    
+    init(name: String, weight: Double, maxSpeed: Double, maxAltitude: Double) {
+        
+        self.maxAltitude = maxAltitude
+        self.altitude = 0
+        
+        super.init(name: name, weight: weight, maxSpeed: maxSpeed)
+    }
+    
+    func takeOff() {
+        
+        guard !inFlight else { return }
+        
+        speed = maxSpeed * 1/10
+        altitude = maxAltitude * 1/10
+    }
+    
+    func land() {
+        
+        speed = 0.0
+        altitude = 0.0
+    }
+    
+    func climb() {
+        
+        guard inFlight else { return }
+        
+        let altitudeIncreaseRate = maxAltitude * 1/10
+        
+        if altitude + altitudeIncreaseRate <= maxAltitude {
+            
+            altitude += altitudeIncreaseRate
+            decelerate()
+            
+        } else {
+            
+            altitude = maxAltitude
+            decelerate()
+        }
+    }
+    
+    func dive() {
+        
+        guard inFlight else { return }
+        
+        let altitudeDecreaseRate = maxAltitude * 1/10
+        
+        if altitude - altitudeDecreaseRate >= 0 {
+            
+            altitude -= altitudeDecreaseRate
+            accelerate()
+            
+        } else {
+            
+            altitude = 0
+            accelerate()
+        }
+        
+    }
+    
+    func bankRight() {
+        
+        guard inFlight else { return }
+        
+        let headingIncreaseRate = 45.0
+        let speedDecreaseRate = speed * 1/10
+        
+        if heading + headingIncreaseRate > 360 {
+            
+            heading = (heading + headingIncreaseRate) - 360
+            speed -= speedDecreaseRate
+            
+        } else {
+            
+            heading += 45
+            speed -= speedDecreaseRate
+        }
+    }
+    
+    func bankLeft() {
+        
+        guard inFlight else { return }
+        
+        let headingDecreaseRate = 45.0
+        let speedDecreaseRate = speed * 1/10
+        
+        if heading - headingDecreaseRate < 0 {
+            
+            heading = (heading - headingDecreaseRate) + 360
+            speed -= speedDecreaseRate
+            
+        } else {
+            
+            heading -= 45
+            speed -= speedDecreaseRate
+        }
+        
+    }
+    
+    
+    
+}
--- CatchEmAll/RaceCar.swift
+++ CatchEmAll/RaceCar.swift
@@ -7,3 +7,78 @@
 //
 
 import Foundation
+
+class RaceCar: Car {
+    
+    let driver: String
+    var sponsors: [String]
+    
+    init(name: String, weight: Double, maxSpeed: Double, transmission: Transmission, cylinders: Int, milesPerGallon: Double, driver: String, sponsors: [String]) {
+        
+        self.driver = driver
+        self.sponsors = sponsors
+        
+        super.init(name: name, weight: weight, maxSpeed: maxSpeed, transmission: transmission, cylinders: cylinders, milesPerGallon: milesPerGallon)
+    }
+    
+    override func accelerate() {
+        
+        let increaseRate = maxSpeed * 1/5
+        
+        if speed + increaseRate <= maxSpeed {
+            
+            speed += increaseRate
+            
+        } else {
+            
+            speed = maxSpeed
+        }
+        
+    }
+    
+    override func decelerate() {
+        
+        let decreaseRate = maxSpeed * 1/5
+        
+        if speed - decreaseRate >= 0 {
+            
+            speed -= decreaseRate
+            
+        } else {
+            
+            speed = 0.0
+        }
+    }
+    
+    func driftRight() {
+        
+        let speedDecreaseRate = speed * 1/4
+        
+        if speed != 0 {
+            
+            heading += 90
+            speed -= speedDecreaseRate
+            
+            if heading >= 360 {
+                
+                heading -= 360
+            }
+        }
+    }
+    
+    func driftLeft() {
+        
+        let speedDecreaseRate = speed * 1/4
+        
+        if speed != 0 {
+            
+            heading -= 90
+            speed -= speedDecreaseRate
+            
+            if heading < 0 {
+                
+                heading += 360
+            }
+        }
+    }
+}
