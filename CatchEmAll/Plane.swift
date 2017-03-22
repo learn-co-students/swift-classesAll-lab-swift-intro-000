b8c98989ec73dc8f8d42351123ec4d5b65813b9c
Brendt Bly <brendtbly@Brendts-MacBook-Pro.local>
2017-03-21 19:41:13 -0500

    Finished but can't figure out why build fails

--- CatchEmAll/Plane.swift
+++ CatchEmAll/Plane.swift
@@ -7,3 +7,91 @@
 //
 
 import Foundation
+
+class Plane: Vehicle {
+    let maxAltitude: Double
+    var altitude: Double
+    var inFlight: Bool {
+        guard altitude > 0 && speed > 0 else {return false}
+        return true
+    }
+    
+    init(maxAltitude: Double) {
+        self.maxAltitude = maxAltitude
+        self.altitude = 0
+        
+        super.init(name: name, weight: weight, maxSpeed: maxSpeed)
+    }
+    
+    func takeOff() {
+        speed = maxSpeed/2
+        altitude = maxAltitude/2
+    }
+    
+    func land() {
+        speed = 0
+        altitude = 0
+    }
+    
+    func climb() {
+        if inFlight && altitude < maxAltitude {
+        decelerate()
+        altitude += maxAltitude/10
+            if altitude > maxAltitude {
+                altitude = maxAltitude
+            }
+        }
+    }
+    
+    func dive() {
+        if inFlight {
+            accelerate()
+            altitude -= maxAltitude/10
+            if altitude < 0 {
+                altitude = 0
+            }
+        }
+    }
+    
+    func bankRight() {
+        if inFlight {
+            heading += 45
+            speed -= speed/10
+            if heading > 360 {
+                heading -= 360
+            }
+        }
+    }
+    
+    func bankLeft() {
+        if inFlight {
+            heading -= 45
+            speed -= speed/10
+            if heading < 0 {
+                heading += 360
+            }
+        }
+    }
+}
+
+/*
+ Next, create a Plane class in Plane.swift. This class should inherit from Vehicle and add three new properties:
+ A constant Double named maxAltitude
+ A variable Double named altitude
+ A calculated Bool named inFlight. This property should return true if both the altitude and speed are greater than 0.
+ Plane should have an initializer that takes four parameters:
+ A String named name
+ A Double named weight
+ A Double named maxSpeed
+ A Double named maxAltitude
+ This initializer should assign the parameter maxAltitude to the maxAltitude property, and also set the altitude parameter to 0. It should then call the superclass's initializer with the remaining parameters.
+ Plane should also add six more methods, each of which take no parameters and return no value:
+ takeOff(), which sets the plane's speed and altitude to ⅒ of their maximums, but only if the plane is not already in flight.
+ land(), which sets the plane's speed and altitude to 0.
+ climb(), which increases the plane's altitude by ⅒ of the plane's maxAltitude and causes the plane to decelerate. It should not set the plane's altitude beyond its maxAltitude, or reduce the speed below 0. The plane can only climb if it is in flight.
+ dive(), which decreases the plane's altitude by ⅒ of the plane's maxAltitude and causes the plane to accelerate. It should not set the plane's altitude below 0 or increase the plane's speed beyond its maximumSpeed. The plane can only dive if it is in flight.
+ bankRight(), which increases the plane's heading by 45 but only decreases the plane's speed by ⅒ of its current value. The plane can only bank if it is in flight.
+ bankLeft(), which decreases the plane's heading by 45 but only decreases the plane's speed by ⅒ of its current value. The plane can only bank if it is in flight.
+ The same rules for handling out of range heading values apply to bankLeft() and bankRight().
+
+ */
--- CatchEmAll/Vehicle.swift
+++ CatchEmAll/Vehicle.swift
@@ -7,3 +7,80 @@
 //
 
 import Foundation
+
+class Vehicle {
+    let name: String
+    let weight: Double
+    let maxSpeed: Double
+    var speed: Double = 0
+    var heading: Double = 0
+    
+    init(name: String, weight: Double, maxSpeed: Double) {
+        self.name = name
+        self.weight = weight
+        self.maxSpeed = maxSpeed
+    }
+    
+    func goFast() {
+        speed = maxSpeed
+        
+    }
+    
+    func halt() {
+        speed = 0
+    }
+    
+    func accelerate() {
+        if speed < maxSpeed/10 {
+        speed += maxSpeed/10
+        } else {
+            speed = maxSpeed
+        }
+    }
+    
+    func decelerate() {
+        let incriment = maxSpeed/10
+        if speed < incriment {
+            speed = 0
+        } else {
+            speed -= incriment
+        }
+    }
+    
+    func turnRight() {
+       heading += 90
+       speed = speed/10
+        if heading > 360 {
+            heading -= 360
+        }
+    }
+    
+    func turnLeft() {
+        heading -= 90
+        speed = speed/10
+        if heading < 0 {
+            heading += 360
+        }
+    }
+    
+}
+
+enum Transmission {
+    case Automatic, Manuel
+}
+/*
+ A constant String named name
+ A constant Double named weight
+ A constant Double named maxSpeed
+ A variable Double named speed
+ A variable Double named heading. (Note that this property should only accept values in the range 0 to 360.)
+ Vehicle should also have an initializer that takes name,weight, andmaxSpeedas parameters, and sets the appropriate properties. It should also setspeedandheading` to 0.0.
+ You should also add these methods to Vehicle. These methods take no parameters and return no values.
+ goFast(), which sets the vehicle's speed to its maxSpeed.
+ halt(), which sets the vehicle's speed to 0.
+ accelerate(), which increases the vehicle's speed by ⅒ its maxSpeed. It cannot increase the vehicle's speed beyond its maxSpeed.
+ decelerate(), which decreases the vehicle's speed by ⅒ its maxSpeed. It cannot decrease the vehicle's speed below 0.
+ turnRight(), which increases the vehicle's heading by 90 and cuts its speed in half. If the vehicle's heading increases beyond 360, it wraps around again (that is, if the heading would be increased to 375, it wraps around to 15 again). The vehicle cannot turn if it is stationary (has a speed of 0).
+ turnLeft() which decreases the vehicle's heading by 90 and cuts its speed in half. If the vehicle's heading decreases below 0, it wraps around again (that is, if it would decrease to -15, it would instead wrap around to 345). The vehicle cannot turn if it is stationery (has a speed of 0).
+
+ */
