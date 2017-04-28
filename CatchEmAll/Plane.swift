f8bc106e32ed25e5b757f430527e01c9bb9826f6
Kelly Lougheed <kellylougheed@gmail.com>
2017-04-28 09:41:05 -0700

    Add answers - won't compile

--- CatchEmAll/Plane.swift
+++ CatchEmAll/Plane.swift
@@ -7,3 +7,63 @@
 //
 
 import Foundation
+
+class Plane: Vehicle {
+    let maxAltitude: Double
+    var altitude: Double = 0
+    var inFlight: Bool {
+        return self.altitude > 0 && self.speed > 0
+    }
+    init(name: String, weight: Double, maxSpeed: Double, maxAltitude: Double) {
+        self.maxAltitude = maxAltitude
+        super.init(name: name, weight: weight, maxSpeed: maxSpeed)
+    }
+    func takeOff() {
+        if !self.inFlight {
+            self.speed += self.maxSpeed * 0.1
+            self.altitude += self.maxAltitude * 0.1
+        }
+    }
+    func land() {
+        self.speed = 0
+        self.altitude = 0
+    }
+    func climb() {
+        if self.inFlight {
+            let increase = self.maxAltitude * 0.1
+            let decrease = self.maxSpeed * 0.1
+            if !(self.altitude + increase > self.maxAltitude) && !(self.speed - decrease < 0) {
+                self.altitude += increase
+                self.decelerate()
+            }
+        }
+    }
+    func dive() {
+        if self.inFlight {
+            let decrease = self.maxAltitude * 0.1
+            let increase = self.maxSpeed * 0.1
+            if !(self.altitude - decrease < 0) && !(self.speed + increase > self.maxSpeed) {
+                self.altitude -= decrease
+                self.accelerate()
+            }
+        }
+    }
+    func bankRight() {
+        if self.inFlight {
+            self.heading += 45
+            if self.heading > 360 {
+                self.heading -= 360
+            }
+            self.speed *= 0.9
+        }
+    }
+    func bankLeft() {
+        if self.inFlight {
+            self.heading -= 45
+            if self.heading < 0 {
+                self.heading += 360
+            }
+            self.speed *= 0.9
+        }
+    }
+}
--- CatchEmAll/RaceCar.swift
+++ CatchEmAll/RaceCar.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+
+class RaceCar: Car {
+    let driver: String
+    var sponsors: [String]
+    init(name: String, weight: Double, maxSpeed: Double, transmission: Transmission, cylinders: Int, milesPerGallon: Double, driver: String, sponsors: [String]) {
+        self.driver = driver
+        self.sponsors = sponsors
+        super.init(name: name, weight: weight, maxSpeed: maxSpeed, transmission: transmission, cylinders: cylinders, milesPerGallon: milesPerGallon)
+    }
+    override func accelerate() {
+        let increase = self.maxSpeed * 0.2
+        if !(self.speed + increase >= self.maxSpeed) {
+            self.speed += increase
+        }
+    }
+    override func decelerate () {
+        let decrease = self.maxSpeed * 0.2
+        if !(self.speed - decrease >= 0) {
+            self.speed -= decrease
+        }
+    }
+    func driftRight() {
+        if self.speed > 0 {
+            super.turnRight()
+            self.speed /= 2
+        }
+    }
+    func driftLeft() {
+        if self.speed > 0 {
+            super.turnLeft()
+            self.speed /= 2
+        }
+    }
+}
