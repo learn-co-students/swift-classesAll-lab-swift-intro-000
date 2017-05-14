b187a2241add7e0d4523306d978e385f41bceaf2
one-for-all <jianan.li@yahoo.com>
2017-05-13 20:57:32 -0700

    completed classes lab problems

--- CatchEmAll/Car.swift
+++ CatchEmAll/Car.swift
@@ -7,3 +7,44 @@
 //
 
 import Foundation
+
+//Next, you need to implement the Car class in Car.swift. Car should inherit from Vehicle. It should add three new properties:
+//A constant Transmission named transmission
+//A constant Int called cylinders
+//A constant Double called milesPerGallon
+//Car should also have an initializer that takes six parameters:
+//A String called name
+//A Double called weight
+//A Double called maxSpeed
+//A Transmission called transmission
+//An Int called cylinders
+//A Double called milesPerGallon
+//The initializer should first set the parameters specific to Car, then call super.init with the parameters required by the Vehicle superclass's initializer. Here's a hint: The Car initializer should look like this:
+//init(name: String, weight: Double, maxSpeed: Double, transmission: Transmission, cylinders: Int, milesPerGallon: Double) {
+//    self.transmission = transmission
+//    self.cylinders = cylinders
+//    self.milesPerGallon = milesPerGallon
+//    super.init(name: name, weight: weight, maxSpeed: maxSpeed)
+//}
+//Finally, Car should add two new methods, which again take no parameters and have no return value:
+//drive(), which simply calls super.accelerate()
+//brake(), which simply calls super.decelerate()
+
+class Car: Vehicle {
+    let transmission: Transmission
+    let cylinders: Int
+    let milesPerGallon: Double
+    init(name: String, weight: Double, maxSpeed: Double, transmission: Transmission, cylinders: Int, milesPerGallon: Double) {
+        self.transmission = transmission
+        self.cylinders = cylinders
+        self.milesPerGallon = milesPerGallon
+        super.init(name: name, weight: weight, maxSpeed: maxSpeed)
+    }
+    func drive() {
+        super.accelerate()
+    }
+    func brake() {
+        super.decelerate()
+    }
+}
+
--- CatchEmAll/Jet.swift
+++ CatchEmAll/Jet.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+
+//Finally, you should implement a class called Jet in Jet.swift. It should inherit from Plane.
+//The Jet class should override two of methods from Plane:
+//Override climb() to increase the jet's altitude by ⅕ of its maxAltitude. Otherwise, the same rules for Plane's climb() should apply.
+//Override dive() to dcrease the jet's altitude by ⅕ of its maxAltitude. Otherwise, the same rules for Plane's dive() should apply.
+//The Jet class should also add one new method, which takes no parameters and returns no value:
+//afterburner(), which sets the jet's speed to twice its maxSpeed, but only if the jet is already flying at its maxSpeed.
+
+class Jet: Plane {
+    override func climb() {
+        if inFlight {
+            altitude += maxAltitude/5
+            if altitude > maxAltitude {
+                altitude = maxAltitude
+            }
+            super.decelerate()
+        }
+    }
+    override func dive() {
+        if inFlight {
+            altitude -= maxAltitude/5
+            if altitude < 0 {
+                altitude = 0
+            }
+            super.accelerate()
+        }
+    }
+    func afterburner() {
+        if inFlight && speed == maxSpeed {
+            speed = maxSpeed*2
+        }
+    }
+}
