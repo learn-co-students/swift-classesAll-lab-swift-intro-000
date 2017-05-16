45c80c4cf21a39c3619c8f9d3451757355bafa4b
kirillsh93 <kirill.shapoval@gmail.com>
2017-05-16 17:06:18 +0300

    inheritance

--- CatchEmAll/Jet.swift
+++ CatchEmAll/Jet.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+
+class Jet: Plane {
+    override func climb() {
+        if inFlight {
+            let tempAlt = maxAltitude / 5
+            if altitude <= maxAltitude - tempAlt {
+                altitude += tempAlt
+            } else {
+                altitude = maxAltitude
+            }
+            
+            decelerate()
+        }
+
+    }
+    
+    override func dive() {
+        if inFlight {
+            let tempAlt = maxAltitude / 5
+            if altitude >= tempAlt {
+                altitude -= tempAlt
+            } else {
+                altitude = 0
+            }
+            
+            accelerate()
+        }
+
+    }
+    
+    func afterburner() {
+        if speed == maxSpeed {
+            speed = maxSpeed * 2
+        }
+    }
+}
