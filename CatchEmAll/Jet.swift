a03fcfc37be61035bea5e7cbdbf0401a9f9829c5
saroar <saroar9@gmail.com>
2017-05-21 21:09:02 +0300

    project solution

--- CatchEmAll/Jet.swift
+++ CatchEmAll/Jet.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+
+class Jet: Plane {
+    override var altitudeInterval: Double {
+        return maxAltitude / 5.0
+    }
+    
+    override var megaSpeed: Double {
+        return maxSpeed * 2.0
+    }
+    
+    override func climb() {
+        guard inFlight else { return }
+        super.altitude += altitudeInterval
+        super.decelerate()
+    }
+    
+    override func dive() {
+        guard inFlight else { return }
+        super.altitude -= altitudeInterval
+        super.accelerate()
+    }
+    
+    func afterburner()  {
+        guard inFlight else { return }
+        guard speed == maxSpeed else { return }
+        speed = maxSpeed * 2.0
+    }
+}
