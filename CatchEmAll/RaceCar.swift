304bc4b90420a9af37fd9d033bfb7d43868b5b61
felixvdl <vandelanottefelix@gmail.com>
2017-06-13 15:45:38 -0400

    finish tests

--- CatchEmAll/RaceCar.swift
+++ CatchEmAll/RaceCar.swift
@@ -7,3 +7,48 @@
 //
 
 import Foundation
+
+class RaceCar: Car {
+    let driver: String
+    var sponsors: [String]
+    
+    init(name: String, weight: Double, maxSpeed: Double, transmission: Transmission, cylinders: Int, milesPerGallon: Double, driver: String, sponsors: [String]) {
+        self.driver = driver
+        self.sponsors = sponsors
+        super.init(name: name, weight: weight, maxSpeed: maxSpeed, transmission: transmission, cylinders: cylinders, milesPerGallon: milesPerGallon)
+    }
+    
+    override func accelerate() {
+        speed += 0.2 * maxSpeed
+        if speed >= maxSpeed {
+            goFast()
+        }
+    }
+    
+    override func decelerate() {
+        speed -= 0.2 * maxSpeed
+        if speed <= 0.0 {
+            halt()
+        }
+    }
+    
+    func driftRight() {
+        if speed > 0 {
+            heading += 90
+            speed *= 0.75
+        }
+        if heading > 360 {
+            heading -= 360
+        }
+    }
+    
+    func driftLeft() {
+        if speed > 0 {
+            heading -= 90
+            speed *= 0.75
+        }
+        if heading < 0.0 {
+            heading += 360
+        }
+    }
+}
--- CatchEmAll/Vehicle.swift
+++ CatchEmAll/Vehicle.swift
@@ -7,3 +7,74 @@
 //
 
 import Foundation
+
+enum Transmission {
+    case Automatic
+    case Manual
+}
+class Vehicle {
+    
+
+    
+    let name: String
+    let weight: Double
+    let maxSpeed: Double
+    var speed: Double
+    var heading: Double
+    
+    init(name: String, weight: Double, maxSpeed: Double) {
+        self.name = name
+        self.weight = weight
+        self.maxSpeed = maxSpeed
+        speed = 0.0
+        heading = 0.0
+    }
+    
+    func goFast() {
+        speed = maxSpeed
+    }
+    
+    func halt() {
+        speed = 0.0
+    }
+    
+    func accelerate() {
+        speed += 0.1 * maxSpeed
+        if speed >= maxSpeed {
+            goFast()
+        }
+    }
+    
+    func decelerate() {
+        speed -= 0.1 * maxSpeed
+        if speed <= 0.0 {
+            halt()
+        }
+    }
+    
+    func turnRight() {
+        if speed > 0 {
+            heading += 90
+            speed /= 2
+            if heading > 360 {
+                heading -= 360
+            }
+        }
+    }
+    
+    func turnLeft() {
+        if speed > 0 {
+            heading -= 90
+            speed /= 2
+            if heading < 0.0 {
+                heading += 360
+            }
+        }
+    }
+    
+
+    
+    
+    
+    
+}
