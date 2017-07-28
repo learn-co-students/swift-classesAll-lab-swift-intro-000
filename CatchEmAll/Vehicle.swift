bd1c9fef0cdb4bf0b997c5ef8221b7c0cfcca30b
JakeCormier <jake.r.cormier@gmail.com>
2017-07-28 12:41:40 -0600

    Complete problems

--- CatchEmAll/Vehicle.swift
+++ CatchEmAll/Vehicle.swift
@@ -7,3 +7,74 @@
 //
 
 import Foundation
+
+class Vehicle {
+    let name : String
+    let weight : Double
+    let maxSpeed : Double
+    var speed = 0.0
+    var heading = 0.0
+    
+    init (name : String, weight : Double, maxSpeed : Double){
+        self.name = name
+        self.weight = weight
+        self.maxSpeed = maxSpeed
+        
+        
+    }
+    func headerControl() -> String {
+        if heading > 360 || heading < 0 {
+            heading = 0.0
+        }
+        return "Heading must be between 0 and 360."
+    }
+    
+    func goFast() {
+        speed = maxSpeed
+    }
+    
+    func halt()  {
+        speed = 0.0
+    }
+    
+    func accelerate()  {
+        speed += maxSpeed*(1/10)
+        if speed > maxSpeed {
+            speed = maxSpeed}
+    }
+    
+    func decelerate() {
+        speed -= maxSpeed*(1/10)
+        if speed < 0 {
+            speed  = 0
+        }
+    }
+    
+    func turnRight() {
+        
+        if speed != 0 {
+            heading += 90
+            speed = speed/2}
+        if heading >= 360 {
+            heading = heading - 360
+        }
+    }
+    
+    func turnLeft () {
+        if speed != 0 {
+            heading -= 90
+            speed = speed/2
+            if heading < 0 {
+                heading = 360 - abs(heading)
+            }
+        }
+    }
+    
+}
+enum Transmission{
+    case Automatic
+    case Manual
+}
+
+
+
