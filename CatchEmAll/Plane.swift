e1f8e49335a4821dcff0a3c6b309ad8ccc87f120
MelissaGarrett <melissa_garrett@hotmail.com>
2017-10-16 15:43:02 -0500

    A lab to practice inheritance.

--- CatchEmAll/Plane.swift
+++ CatchEmAll/Plane.swift
@@ -7,3 +7,94 @@
 //
 
 import Foundation
+
+class Plane : Vehicle {
+    let maxAltitude: Double
+    var altitude: Double
+    var inFlight: Bool {
+        if altitude > 0 && speed > 0 {
+            return true
+        }
+        return false
+    }
+    
+    init(name: String, weight: Double, maxSpeed: Double, maxAltitude: Double) {
+        self.maxAltitude = maxAltitude
+        self.altitude = 0
+        
+        super.init(name: name, weight: weight, maxSpeed: maxSpeed)
+    }
+    
+    func takeOff() {
+        if self.inFlight == false {
+            speed = maxSpeed * 0.10
+            self.altitude = self.maxAltitude * 0.10
+        }
+    }
+    
+    func land() {
+        speed = 0
+        self.altitude = 0
+    }
+    
+    func climb() {
+        if self.inFlight == true {
+            self.altitude += self.maxAltitude * 0.10
+            if self.altitude > self.maxAltitude {
+                self.altitude = self.maxAltitude
+            }
+            
+            decelerate()
+        }
+    }
+    
+    func dive() {
+        if self.inFlight == true {
+            self.altitude -= self.maxAltitude * 0.10
+            if self.altitude < 0 {
+                self.altitude = 0
+            }
+            
+            accelerate()
+        }
+    }
+    
+    func bankRight() {
+        if self.inFlight == true {
+            heading += 45
+            if heading > 360 {
+                heading -= 360
+            }
+            
+            speed -= speed * 0.10
+        }
+    }
+    
+    func bankLeft() {
+        if self.inFlight == true {
+            heading -= 45
+            if heading < 0 {
+                heading = 360 - abs(heading)
+            }
+            
+            speed -= speed * 0.10
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- CatchEmAll/Vehicle.swift
+++ CatchEmAll/Vehicle.swift
@@ -7,3 +7,89 @@
 //
 
 import Foundation
+
+class Vehicle {
+    let name: String
+    let weight: Double
+    let maxSpeed: Double
+    var speed: Double
+    var heading: Double
+    
+    init(name: String, weight: Double, maxSpeed: Double) {
+        self.name = name
+        self.weight = weight
+        self.maxSpeed = maxSpeed
+        self.speed = 0.0
+        self.heading = 0.0
+    }
+    
+    func goFast() {
+        self.speed = self.maxSpeed
+    }
+    
+    func halt() {
+        self.speed = 0.0
+    }
+    
+    func accelerate() {
+        self.speed += self.maxSpeed * 0.10
+        
+        if self.speed > self.maxSpeed {
+            self.speed = self.maxSpeed
+        }
+    }
+    
+    func decelerate() {
+        self.speed -= self.maxSpeed * 0.10
+        
+        if self.speed < 0 {
+            self.speed = 0
+        }
+    }
+    
+    func turnRight() {
+        if self.speed == 0 {
+            return
+        }
+        
+        self.heading += 90
+        if self.heading > 360 {
+            self.heading -= 360
+        }
+        
+        self.speed *= 0.5
+    }
+    
+    func turnLeft() {
+        if self.speed == 0 {
+            return
+        }
+        
+        self.heading -= 90
+        if self.heading < 0 {
+            self.heading = 360 - abs(self.heading)
+        }
+        
+        self.speed *= 0.5
+    }
+}
+
+enum Transmission {
+    case Automatic
+    case Manual
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
