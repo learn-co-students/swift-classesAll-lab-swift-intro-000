d02f926047570f4f8f08d1f925c5e49583fba859
chenjm1960 <chenjm1960@yahoo.com>
2018-03-01 00:21:53 -0800

    finished.

--- CatchEmAll/Jet.swift
+++ CatchEmAll/Jet.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+
+class Jet:Plane {
+    
+    override func climb() {
+        if inFlight == true {
+            if altitude < (maxAltitude) && speed > 0.0 {
+                altitude+=((1/5)*maxAltitude)
+                if altitude > maxAltitude {
+                    altitude = maxAltitude
+                }
+                decelerate()
+            }
+        }
+    }
+    override func dive() {
+        if inFlight == true {
+            if altitude < (maxAltitude) && speed > 0.0 {
+                altitude-=((1/5)*maxAltitude)
+                if altitude < 0.0 {
+                    altitude = 0.0
+                }
+                accelerate()
+            }
+        }
+    }
+    
+    func afterburner() {
+        
+        if inFlight == true && speed == maxSpeed {
+            speed = 2*maxSpeed
+        }
+        
+    }
+    
+}
--- CatchEmAll/Vehicle.swift
+++ CatchEmAll/Vehicle.swift
@@ -7,3 +7,82 @@
 //
 
 import Foundation
+
+enum Transmission {
+    case Automatic,Manual
+}
+
+
+class Vehicle {
+    
+    let name:String
+    let weight:Double
+    let maxSpeed:Double
+    var speed:Double = 0.0
+    var heading:Double = 0.0
+    
+    init(name:String,weight:Double,maxSpeed:Double) {
+        self.name = name
+        self.weight = weight
+        self.maxSpeed = maxSpeed
+        
+    }
+    func goFast() {
+        speed = maxSpeed
+    }
+    func halt() {
+        speed = 0.0
+    }
+    func accelerate() {
+        if speed < maxSpeed {
+            speed = speed + ((1/10)*maxSpeed)
+            if speed > maxSpeed {
+                speed = maxSpeed
+            }
+        }
+    }
+    func decelerate() {
+        if speed > 0.0 {
+            speed = speed - ((1/10)*maxSpeed)
+            if speed < 0.0 {
+                speed = 0.0
+            }
+        }
+    }
+    func turnRight() {
+        if heading >= 0.0 && heading <= 270.0 && speed != 0.0 {
+            heading+=90.0
+            speed = speed/2
+        } else if heading > 270.0 && speed != 0.0 {
+            heading = heading + 90.0 - 360.0
+            speed = speed/2
+        }
+    }
+    func turnLeft() {
+        if heading >= 90.0 && heading <= 360.0 && speed != 0.0 {
+            heading-=90.0
+            speed = speed/2
+        } else if heading < 90.0 && speed != 0.0 {
+            heading = 360 - (90.0 - heading)
+            speed = speed/2
+        }
+    }
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
